89dda12f060d114e6b305631c63e48dbac02e92c
Rodrigo Maciel <rodrigo.maciel@der.rj>
2020-10-24 14:09:57 -0300

    acerto ortográfico e layout do roster

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
   <!-- start concil section -->
   <section class="concil" id="concil">
     <div class="title">
-      <p class="mean_title">o concelho</p>
+      <p class="mean_title">o conselho</p>
       <p class="sub_title">
         <em>"Na batalha, o primeiro passo para a vitória é o desejo de vencer!"</em>
       </p>
@@ -114,10 +114,10 @@
   <!-- start roster section -->
   <section class="roster" id="roster">
     <div class="title">
-      <p class="mean_title">roster</p>
-      {{-- <p class="sub_title">
-        <em>&nbsp;</em>
-      </p> --}}
+      {{-- <p class="mean_title">roster</p> --}}
+      <p class="sub_title">
+        <em>nossa equipe</em>
+      </p>
     </div>
     <div class="grid-x grid-margin-x roster_list">
       <!-- start roster list -->
@@ -146,14 +146,14 @@
       <p class="sub_title">&nbsp;</p>
     </div> --}}
     <div class="grid-x grid-margin-x">
-      <div class="cell large-7">
+      <div class="cell small-12 medium-6 large-7">
         <p class="assign">Se você deseja fazer parte do nosso grupo (espero que o zeto faça um texto melhor, que
           cative
           alguém) <a href="#">clique aqui</a> e faça parte do nosso time, ou então nos deixe a sua
           mensagem de apoio
           aqui ao lado.</p>
       </div>
-      <div class="cell large-2 large-offset-2">
+      <div class="cell small-12 medium-5 large-3 large-offset-1">
         <div class="translucent-form-overlay">
           <form>
             <p>Ou fale conosco</p>
